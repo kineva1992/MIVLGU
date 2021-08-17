63154b1731903dab30b5bea0bd32137daf310b88
alexandex <kienva1992@gmail.com>
2021-08-17 19:16:44 +0300

    Добавьте файлы проекта.

---
+++ 3 course/SoftWare and SAPR/EHkspertnayaSistema/EHkspertnayaSistema/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace EHkspertnayaSistema
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(959, 539);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Controls.Add(this.panel1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(951, 510);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Экспертная система";
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(868, 469);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 35);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Выход";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(9, 79);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(934, 160);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.panel1.Controls.Add(this.radioButton3);
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Location = new System.Drawing.Point(278, 245);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(365, 112);
+            this.panel1.TabIndex = 4;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(101, 75);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(17, 16);
+            this.radioButton3.TabIndex = 3;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(101, 48);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(17, 16);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(101, 21);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(17, 16);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button1.Location = new System.Drawing.Point(395, 467);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Начать сначала";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridView1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(951, 510);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Таблица переходов";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(945, 504);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.dataGridView2);
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(951, 510);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Таблица Вопросы-Ответы";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView2.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.RowHeadersVisible = false;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(945, 504);
+            this.dataGridView2.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(959, 539);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ 3 course/SoftWare and SAPR/EHkspertnayaSistema/EHkspertnayaSistema/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace EHkspertnayaSistema
+{
+    public partial class Form1 : Form
+    {
+        byte NachalnoeSostoyanie = 0;
+        byte TekushcheeSostoyanie = 0;
+        byte KonecPoiska = 0;
+        Font fnt = new Font("Times New Roman", 14);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PostroitTablicuPerekhodov();
+            PostroitTablicuVoprosOtvet();
+            Reshit();
+        }
+        private void PostroitTablicuPerekhodov()
+        {
+            dataGridView1.ColumnCount = 4;
+            dataGridView1.ColumnHeadersVisible = true;
+            DataGridViewCellStyle columnHeaderStyle = new DataGridViewCellStyle();
+            columnHeaderStyle.BackColor = Color.Beige;
+            columnHeaderStyle.Font = new Font("Verdana", 8, FontStyle.Bold);
+            dataGridView1.ColumnHeadersDefaultCellStyle = columnHeaderStyle;
+            dataGridView1.Columns[0].Name = "Начальное состояние";
+            dataGridView1.Columns[0].Width = 170;
+            dataGridView1.Columns[1].Name = "Конечное состояние";
+            dataGridView1.Columns[1].Width = 160;
+            dataGridView1.Columns[2].Name = "Конец поиска";
+            dataGridView1.Columns[2].Width = 110;
+            dataGridView1.Columns[3].Name = "Ответ пользователя";
+            string[] row1 = new string[] { "0", "1", "0", "Хочу найти работу" };
+            string[] row2 = new string[] { "0", "2", "0", "Для саморазвития" };
+            string[] row3 = new string[] { "1", "3", "0", "В компании" };
+            string[] row4 = new string[] { "1", "11", "0", "Стартап" };
+            string[] row5 = new string[] { "2", "4", "0", "Высокоуровневый" };
+            string[] row6 = new string[] { "2", "5", "0", "«Железо»" };
+            string[] row7 = new string[] { "3", "6", "1", "Google" };
+            string[] row8 = new string[] { "3", "7", "1", "Windows" };
+            string[] row9 = new string[] { "3", "8", "1", "Apple" };
+            string[] row10 = new string[] { "4", "9", "1", "Объекты" };
+            string[] row11 = new string[] { "4", "10", "1", "Функции" };
+            string[] row12 = new string[] { "5", "12", "1", "Максимальные возможности" };
+            string[] row13 = new string[] { "5", "13", "1", "Более понятный язык" };
+            string[] row14 = new string[] { "11", "14", "0", "Мобильные приложения" };
+            string[] row15 = new string[] { "11", "15", "0", "Web" };
+            string[] row16 = new string[] { "14", "16", "1", "iOS" };
+            string[] row17 = new string[] { "14", "17", "1", "Android" };
+            string[] row18 = new string[] { "15", "18", "0", "Интерактивен" };
+            string[] row19 = new string[] { "15", "19", "1", "Контент" };
+            string[] row20 = new string[] { "18", "20", "1", "Новая технология" };
+            string[] row21 = new string[] { "18", "21", "1", "Проверенный инструмент" };
+            object[] rows = new object[] { row1, row2, row3, row4, row5, row6, row7, row8, row9, row10, row11,
+                row12,row13, row14, row15, row16, row17, row18, row19, row20, row21};
+            foreach (string[] rowArray in rows)
+            {
+                dataGridView1.Rows.Add(rowArray);
+            }
+        }
+        private void PostroitTablicuVoprosOtvet()
+        {
+            dataGridView2.ColumnCount = 2;
+            dataGridView2.Columns[0].Width = 50;
+            string[] row1 = new string[] { "0", "Для чего вы хотите изучать программирование?" };
+            string[] row2 = new string[] { "1", "Работа в компании, или свой стартап?" };
+            string[] row3 = new string[] { "2", "Высокоуровневый или на уровне «железа»?" };
+            string[] row4 = new string[] { "3", "В какой компании вы хотите работать?" };
+            string[] row5 = new string[] { "6", "Вам подойдёт язык программирования Python." };
+            string[] row6 = new string[] { "7", "Вам подойдёт язык программирования С#." };
+            string[] row7 = new string[] { "8", "Вам подойдёт язык программирования Objective-C." };
+            string[] row8 = new string[] { "11", "Мобильные приложения или web?" };
+            string[] row9 = new string[] { "14", "Какая ОС?"};
+            string[] row10 = new string[] { "16", "Вам подойдёт язык программирования Objective-C." };
+            string[] row11 = new string[] { "17", "Вам подойдёт язык программирования Java." };
+            string[] row12 = new string[] { "15", "Контент или интерактив?" };
+            string[] row13 = new string[] { "18", "Проверенный инструмент или новая техенология?" };
+            string[] row14 = new string[] { "20", "Вам подойдёт язык программирования Node.js." };
+            string[] row15 = new string[] { "21", "Вам подойдёт язык программирования Ruby." };
+            string[] row16 = new string[] { "19", "Вам подойдёт язык программирования Django." };
+            string[] row17 = new string[] { "4", "Объекты или функции?" };
+            string[] row18 = new string[] { "9", "Вам подойдёт язык программирования Smalltalk." };
+            string[] row19 = new string[] { "10", "Вам подойдёт язык программирования Lisp." };
+            string[] row20 = new string[] { "5", "Что для вас приоритетней максимальные возможности или болей понятный язык?" };
+            string[] row21 = new string[] { "12", " Вам подойдёт язык программирования Assembler" };
+            string[] row22 = new string[] { "13", "Вам подойдёт язык программирования С." };
+            object[] rows = new object[] { row1, row2, row3, row4, row5, row6, row7, row8, row9, row10, row11,
+                row12,row13, row14, row15, row16, row17, row18, row19, row20, row21, row22};
+            foreach (string[] rowArray in rows)
+            {
+                dataGridView2.Rows.Add(rowArray);
+            }
+        }
+        private void Reshit()
+        {
+            for (int i = 0; i < dataGridView2.RowCount; i++)
+            {
+                if (TekushcheeSostoyanie == Convert.ToByte(dataGridView2[0, i].Value))
+                {
+                    ZadatVopros(dataGridView2[1, i].FormattedValue.ToString());
+                    break;
+                }
+            }
+            if(KonecPoiska == 0)
+            {
+                Otvety();
+            }
+            else
+            {
+                radioButton1.Visible = false;
+                radioButton2.Visible = false;
+                radioButton3.Visible = false;
+            }
+        }
+        private void ZadatVopros(string text)
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics g = Graphics.FromImage(bmp);
+            StringFormat sf = new StringFormat();
+            Rectangle r = new Rectangle(0, 0, pictureBox1.Width, pictureBox1.Height);
+            sf.LineAlignment = StringAlignment.Center;
+            sf.Alignment = StringAlignment.Center;
+            g.DrawString(text, fnt, Brushes.Black, r, sf);
+            pictureBox1.Image = bmp;
+        }
+        private void Otvety()
+        {
+            if(NachalnoeSostoyanie == 0)
+            {
+                radioButton1.Text = dataGridView1[3, 0].FormattedValue.ToString();
+                radioButton2.Text = dataGridView1[3, 1].FormattedValue.ToString();
+                radioButton3.Visible = false;
+            }
+            else if(NachalnoeSostoyanie == 1)
+            {
+                radioButton1.Text = dataGridView1[3, 2].FormattedValue.ToString();
+                radioButton2.Text = dataGridView1[3, 3].FormattedValue.ToString();
+                radioButton3.Visible = false;
+            }
+            else if(NachalnoeSostoyanie == 2)
+            {
+                radioButton1.Text = dataGridView1[3, 4].FormattedValue.ToString();
+                radioButton2.Text = dataGridView1[3, 5].FormattedValue.ToString();
+                radioButton3.Visible = false;
+            }
+            else if(NachalnoeSostoyanie == 3)
+            {
+                radioButton1.Text = dataGridView1[3, 6].FormattedValue.ToString();
+                radioButton2.Text = dataGridView1[3, 7].FormattedValue.ToString();
+                radioButton3.Visible = true;
+                radioButton3.Text = dataGridView1[3, 8].FormattedValue.ToString();
+            }
+            else if(NachalnoeSostoyanie == 4)
+            {
+                radioButton1.Text = dataGridView1[3, 9].FormattedValue.ToString();
+                radioButton2.Text = dataGridView1[3, 10].FormattedValue.ToString();
+                radioButton3.Visible = false;
+            }
+            else if (NachalnoeSostoyanie == 5)
+            {
+                radioButton1.Text = dataGridView1[3, 11].FormattedValue.ToString();
+                radioButton2.Text = dataGridView1[3, 12].FormattedValue.ToString();
+                radioButton3.Visible = false;
+            }
+            else if (NachalnoeSostoyanie == 11)
+            {
+                radioButton1.Text = dataGridView1[3, 13].FormattedValue.ToString();
+                radioButton2.Text = dataGridView1[3, 14].FormattedValue.ToString();
+                radioButton3.Visible = false;
+            }
+            else if (NachalnoeSostoyanie == 14)
+            {
+                radioButton1.Text = dataGridView1[3, 15].FormattedValue.ToString();
+                radioButton2.Text = dataGridView1[3, 16].FormattedValue.ToString();
+                radioButton3.Visible = false;
+            }
+            else if (NachalnoeSostoyanie == 15)
+            {
+                radioButton1.Text = dataGridView1[3, 17].FormattedValue.ToString();
+                radioButton2.Text = dataGridView1[3, 18].FormattedValue.ToString();
+                radioButton3.Visible = false;
+            }
+            else if (NachalnoeSostoyanie == 18)
+            {
+                radioButton1.Text = dataGridView1[3, 19].FormattedValue.ToString();
+                radioButton2.Text = dataGridView1[3, 20].FormattedValue.ToString();
+                radioButton3.Visible = false;
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            Perejti(radioButton1.Text);
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            Perejti(radioButton2.Text);
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            Perejti(radioButton3.Text);
+        }
+        private void Perejti(string otvet)
+        {
+            
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                radioButton1.Checked = false;
+                radioButton2.Checked = false;
+                radioButton3.Checked = false;
+                if (TekushcheeSostoyanie == Convert.ToByte(dataGridView1[0, i].Value))
+                {
+                    if(otvet == dataGridView1[3, i].Value.ToString())
+                    {
+                        TekushcheeSostoyanie = Convert.ToByte(dataGridView1[1, i].Value);
+                        NachalnoeSostoyanie = Convert.ToByte(dataGridView1[1, i].Value);
+                        KonecPoiska = Convert.ToByte(dataGridView1[2, i].Value);
+                        Reshit();
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            NachalnoeSostoyanie = 0;
+            TekushcheeSostoyanie = 0;
+            KonecPoiska = 0;
+            radioButton1.Visible = true;
+            radioButton2.Visible = true;
+            Reshit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
