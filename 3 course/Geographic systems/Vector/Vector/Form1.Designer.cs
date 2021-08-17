2c370d900de6bf594607dcd1a19b45f6ee5010c5
alexandex <kienva1992@gmail.com>
2021-08-17 19:29:25 +0300

    й

---
+++ 3 course/Geographic systems/Vector/Vector/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿using System;
+
+namespace Vector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.typeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.xDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.yDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.myBaseBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.myBaseDataSet = new Vector.MyBaseDataSet();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.myBaseTableAdapter = new Vector.MyBaseDataSetTableAdapters.MyBaseTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myBaseBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myBaseDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Открыть файл";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(648, 448);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(237, 515);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.CausesValidation = false;
+            this.label2.Location = new System.Drawing.Point(374, 515);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.typeDataGridViewTextBoxColumn,
+            this.xDataGridViewTextBoxColumn,
+            this.yDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.myBaseBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(675, 68);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(164, 380);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // typeDataGridViewTextBoxColumn
+            // 
+            this.typeDataGridViewTextBoxColumn.DataPropertyName = "Type";
+            this.typeDataGridViewTextBoxColumn.HeaderText = "Type";
+            this.typeDataGridViewTextBoxColumn.Name = "typeDataGridViewTextBoxColumn";
+            this.typeDataGridViewTextBoxColumn.Width = 60;
+            // 
+            // xDataGridViewTextBoxColumn
+            // 
+            this.xDataGridViewTextBoxColumn.DataPropertyName = "X";
+            this.xDataGridViewTextBoxColumn.HeaderText = "X";
+            this.xDataGridViewTextBoxColumn.Name = "xDataGridViewTextBoxColumn";
+            this.xDataGridViewTextBoxColumn.Width = 30;
+            // 
+            // yDataGridViewTextBoxColumn
+            // 
+            this.yDataGridViewTextBoxColumn.DataPropertyName = "Y";
+            this.yDataGridViewTextBoxColumn.HeaderText = "Y";
+            this.yDataGridViewTextBoxColumn.Name = "yDataGridViewTextBoxColumn";
+            this.yDataGridViewTextBoxColumn.Width = 30;
+            // 
+            // myBaseBindingSource
+            // 
+            this.myBaseBindingSource.DataMember = "MyBase";
+            this.myBaseBindingSource.DataSource = this.myBaseDataSet;
+            // 
+            // myBaseDataSet
+            // 
+            this.myBaseDataSet.DataSetName = "MyBaseDataSet";
+            this.myBaseDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Дом",
+            "Дорога"});
+            this.comboBox1.Location = new System.Drawing.Point(675, 41);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(164, 21);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.Text = "Тип";
+            this.comboBox1.SelectionChangeCommitted += new System.EventHandler(this.comboBox1_SelectionChangeCommitted);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(759, 466);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(78, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Очистить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // myBaseTableAdapter
+            // 
+            this.myBaseTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(851, 499);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myBaseBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myBaseDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+           
+            
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button3;
+        private MyBaseDataSet myBaseDataSet;
+        private System.Windows.Forms.BindingSource myBaseBindingSource;
+        private MyBaseDataSetTableAdapters.MyBaseTableAdapter myBaseTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn typeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn xDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn yDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ 3 course/Geographic systems/Vector/Vector/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vector
+{
+    public partial class Form1 : Form
+    {
+
+        public int x_MouseDown_1, y_MouseDown_1, x_MouseDown_2, y_MouseDown_2; //дом
+        public int x_MouseDown_11, y_MouseDown_11, x_MouseDown_22, y_MouseDown_22;//дорога 
+        bool OffOn = false; //переключатель условия
+        int K1 = 0;
+        int K2 = 0;
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "myBaseDataSet.MyBase". При необходимости она может быть перемещена или удалена.
+            //this.myBaseTableAdapter.Fill(this.myBaseDataSet.MyBase);
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+            //Открытие файла
+            using (OpenFileDialog ofd = new OpenFileDialog()) 
+            {
+                ofd.Filter = "Image Files(*.BMP;*.JPG;*.GIF)|*.BMP;*.JPG;*.GIF|All files (*.*)|*.*";
+                ofd.RestoreDirectory = true;
+                try
+                {
+                    
+                    if (ofd.ShowDialog() == DialogResult.OK)
+                    {
+                        
+                        pictureBox1.Image = Image.FromFile(ofd.FileName);
+
+
+                    }
+
+                }
+
+                catch
+                {
+                    MessageBox.Show("Выберити тип jpeg");
+                }
+
+            }
+        }
+                
+        private void DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+            g.Clear(BackColor);
+            K1 = 0;
+            K2 = 0;
+        }
+                
+        private void pictureBox1_MouseClick_1(object sender, MouseEventArgs e)
+        {
+           
+            var Type = comboBox1.Text;
+            var location = e.Location;
+            int X = location.X;
+            int Y = location.Y;
+            Graphics g = pictureBox1.CreateGraphics();
+            if (comboBox1.SelectedIndex < 0)
+            {
+                MessageBox.Show("Выбери Тип");
+            }
+            if (comboBox1.SelectedIndex == 0)
+            {
+                //dataGridView1.Rows.Add(Type, X, Y);
+                K1++;//высчитывает количество нажатий
+                Pen myPen = new Pen(Color.Red, 3);
+                x_MouseDown_2 = e.X;
+                y_MouseDown_2 = e.Y;
+                if (OffOn == false)
+                {
+                    x_MouseDown_1 = x_MouseDown_2;
+                    y_MouseDown_1 = y_MouseDown_2;
+                    OffOn = true;
+                }
+                else
+                {
+                    g.DrawLine(myPen, x_MouseDown_1, y_MouseDown_1, x_MouseDown_2, y_MouseDown_2);
+                    x_MouseDown_1 = x_MouseDown_2;
+                    y_MouseDown_1 = y_MouseDown_2;
+                    if (K1 == 5)// если нажатий 5 то прекращаем рисовать и сбрасываем счетчик нажатий
+                    {
+                        OffOn = false; K1 = 0;
+                    }
+                    else
+                    {
+
+                    }                    
+                }
+            }
+            if (comboBox1.SelectedIndex == 1)
+            {
+               //dataGridView1.Rows.Add(Type, X, Y);
+                K2++;//высчитывает количество нажатий
+                Pen myPen = new Pen(Color.Blue, 3);
+                x_MouseDown_22 = e.X;
+                y_MouseDown_22 = e.Y;
+                if (OffOn == false)
+                {
+                    x_MouseDown_11 = x_MouseDown_22;
+                    y_MouseDown_11 = y_MouseDown_22;
+                    OffOn = true;
+                }
+                else
+                {
+                    g.DrawLine(myPen, x_MouseDown_11, y_MouseDown_11, x_MouseDown_22, y_MouseDown_22);
+                    x_MouseDown_11 = x_MouseDown_22;
+                    y_MouseDown_11 = y_MouseDown_22;
+                    if (K2 == 5)// если нажатий 5 то прекращаем рисовать и сбрасываем счетчик нажатий
+                    {
+                        OffOn = false; K2 = 0;
+                    }
+                    else
+                    {
+
+                    }
+                }
+            }
+        }
+        
+         
+        private void comboBox1_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            K1 = 0;
+            K2 = 0;
+        }
+    }
+}
